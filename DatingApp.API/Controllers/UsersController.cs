6e3835a7392b033aef565bbee3e9742dd425b734
milos_stolica <milos_stolica@hotmail.com>
2022-04-01 07:45:01 +0200

    Setting up .net part of aplication

---
+++ DatingApp.API/Controllers/UsersController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using DatingApp.API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace DatingApp.API.Controllers
+{
+    [ApiController]
+    [Route("api/users")]
+    public class UsersController : ControllerBase
+    {
+        private readonly ILogger<UsersController> logger;
+        private readonly DataContext db;
+
+        public UsersController(ILogger<UsersController> logger, DataContext db)
+        {
+            this.logger = logger;
+            this.db = db;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            //logger.LogInformation($"Thread id {Thread.CurrentThread.ManagedThreadId}.");
+            return await db.Users.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            return await db.Users.FindAsync(id);
+        }
+    }
+}
