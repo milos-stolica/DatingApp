6dbf9888bb7e252e42e8a8c111d355441030a3d6
Milos Stolica <milos.stolica@hotmail.com>
2022-04-07 19:35:41 +0200

    Create client login and register

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { User } from '../models/user';
+import { AccountService } from '../services/account.service';
 
 @Component({
   selector: 'app-root',
@@ -11,25 +13,14 @@ export class AppComponent implements OnInit {
   title = 'The Dating App';
   users: any;
 
-  constructor(private http : HttpClient){}
+  constructor(private accountService: AccountService) {}
 
   ngOnInit(): void {
-    this.getUsers();
+    this.setCurrentUser();
   }
 
-  private getUsers() : void {
-    this.http.get('https://localhost:5001/api/users').subscribe(this.getUsersSuccess.bind(this), this.getUsersError.bind(this))
-  }
-
-  private getUsersSuccess(response: any) : void {
-    this.users = response;
-    for(let i = 0; i < this.users.length; i++)
-    {
-      console.log(this.users[i].userName);
-    }
-  }
-
-  private getUsersError(error: any) : void {
-    console.log(error);
+  setCurrentUser() {
+    const user: User = JSON.parse(localStorage.getItem('user'));
+    this.accountService.setCurrentUser(user);
   }
 }
